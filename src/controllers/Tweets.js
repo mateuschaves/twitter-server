b7add6fd09e605412f3c3bf0a2a30e24188e7dec
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-06-02 23:03:05 -0300

    bug fixed

--- src/controllers/Tweets.js
+++ src/controllers/Tweets.js
@@ -33,53 +33,92 @@ module.exports = {
             })
             .then(({ translations }) => {
               req.body.content = translations[0].translation;
+              const nlu = new NaturalLanguageUnderstandingV1({
+                iam_apikey:
+                  process.env.NATURAL_LANGUAGE_UNDERSTANDING_IAM_APIKEY,
+                version: "2018-04-05",
+                url: process.env.NATURAL_LANGUAGE_UNDERSTANDING_URL
+              });
+              nlu
+                .analyze({
+                  html: req.body.content,
+                  features: {
+                    emotion: {}
+                  }
+                })
+                .then(async result => {
+                  const {
+                    emotion: {
+                      document: {
+                        emotion: { sadness, joy, fear, anger }
+                      }
+                    }
+                  } = result;
+
+                  const scores = [sadness, joy, fear, anger];
+                  for (let i = 0; i < 4; i++) {
+                    if (scores[i] > emotion.score) {
+                      emotion.score = scores[i];
+                      emotion.emotion = tags[i];
+                      req.body.emotion = emotion;
+                    }
+                  }
+                  const tweet = await Tweet.create(req.body);
+                  req.io.emit("tweet", tweet);
+                  return res.json(tweet);
+                })
+                .catch(async _ => {
+                  const tweet = await Tweet.create(req.body);
+                  req.io.emit("tweet", tweet);
+                  return res.json(tweet);
+                });
             })
             .catch(err => {
               console.log("error:", err);
+            });
+        } else {
+          const nlu = new NaturalLanguageUnderstandingV1({
+            iam_apikey: process.env.NATURAL_LANGUAGE_UNDERSTANDING_IAM_APIKEY,
+            version: "2018-04-05",
+            url: process.env.NATURAL_LANGUAGE_UNDERSTANDING_URL
+          });
+          nlu
+            .analyze({
+              html: req.body.content,
+              features: {
+                emotion: {}
+              }
+            })
+            .then(async result => {
+              const {
+                emotion: {
+                  document: {
+                    emotion: { sadness, joy, fear, anger }
+                  }
+                }
+              } = result;
+
+              const scores = [sadness, joy, fear, anger];
+              for (let i = 0; i < 4; i++) {
+                if (scores[i] > emotion.score) {
+                  emotion.score = scores[i];
+                  emotion.emotion = tags[i];
+                  req.body.emotion = emotion;
+                }
+              }
+              const tweet = await Tweet.create(req.body);
+              req.io.emit("tweet", tweet);
+              return res.json(tweet);
+            })
+            .catch(async _ => {
+              const tweet = await Tweet.create(req.body);
+              req.io.emit("tweet", tweet);
+              return res.json(tweet);
             });
         }
       })
       .catch(err => {
         console.log("error:", err);
-      });
-
-    const nlu = new NaturalLanguageUnderstandingV1({
-      iam_apikey: process.env.NATURAL_LANGUAGE_UNDERSTANDING_IAM_APIKEY,
-      version: "2018-04-05",
-      url: process.env.NATURAL_LANGUAGE_UNDERSTANDING_URL
-    });
-    nlu
-      .analyze({
-        html: req.body.content,
-        features: {
-          emotion: {}
-        }
-      })
-      .then(async result => {
-        const {
-          emotion: {
-            document: {
-              emotion: { sadness, joy, fear, anger }
-            }
-          }
-        } = result;
-
-        const scores = [sadness, joy, fear, anger];
-        for (let i = 0; i < 4; i++) {
-          if (scores[i] > emotion.score) {
-            emotion.score = scores[i];
-            emotion.emotion = tags[i];
-            req.body.emotion = emotion;
-          }
-        }
-        const tweet = await Tweet.create(req.body);
-        req.io.emit("tweet", tweet);
-        return res.json(tweet);
-      })
-      .catch(async _ => {
-        const tweet = await Tweet.create(req.body);
-        req.io.emit("tweet", tweet);
-        return res.json(tweet);
       });
   }
 };
